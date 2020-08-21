08afb90e595ecddad7f56fbe2c92bddb0c692f5b
“hxm” <1725781471@qq.com>
2020-08-21 10:36:28 +0800

    33

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,141 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-container style="height:100%">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="12" class="logo">
+          <i class="el-icon-connection"></i>斌斌粉丝后台管理系统
+        </el-col>
+        <el-col :span="12" style="text-align:right">
+          <el-button type="text">注册</el-button>
+          <el-button type="text">登录</el-button>
+        </el-col>
+      </el-row>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          style="height:100%"
+          :default-active="activeIndex"
+          mode="vertical"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ff0"
+          @select="changeMenu"
+          :default-openeds="openMenu"
+          router
+        >
+          <template v-for="item in menu">
+            <el-menu-item :index="item.path" :key="item.path"  v-if="!item.submenu">
+              <i :class="item.icon" style="color:#fff"></i>
+              {{item.text}}
+            </el-menu-item>
+            <el-submenu :key="item.path" :index="item.path" v-else>
+              <template v-slot:title>
+                <i :class="item.icon" style="color:#fff"></i>{{item.text}}
+              </template>
+              <el-menu-item :key="sub.path" :index="item.path+sub.path" v-for="sub in item.submenu">{{sub.text}}</el-menu-item>
+            </el-submenu>
+          </template>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+          <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+          <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+        </el-breadcrumb>
+        <div style="padding:15px 0;">
+          <router-view />
+        </div>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name:"App",
+  data(){
+    return{
+      activeIndex:"/home",
+      openMenu:[],
+      menu:[
+        {
+          text:"首页",
+          path:"/home",
+          icon:"el-icon-s-home"
+        },
+        {
+          text: "用户管理",
+          path: "/user",
+          icon: "el-icon-user-solid",
+          submenu: [
+            {
+              text: "添加用户",
+              path: "/add",
+            },
+            {
+              text: "用户列表",
+              path: "/list",
+            },
+          ],
+        },
+        {
+          text: "商品管理",
+          path: "/goods",
+          icon: "el-icon-grape",
+        },
+        {
+          text: "订单管理",
+          path: "/order",
+          icon: "el-icon-s-order",
+        }
+      ],
+      currentIndex:0,
+    }
+  },
+  methods: {
+    goto(path,idx){
+      console.log(this.$router);
+      this.$router.push(path)
+      this.currentIndex = idx
+    },
+    back(){
+      this.$router.back()
+    },
+    changeMenu(path){
+      this.activeIndex = path
+    }
+  },
+  components:{}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  html{height:100%}
+body {
+  margin: 0;
+  height:100%
+}
+.header {
+  line-height: 60px;
+  color: #fff;
+  background-color: rgba(84, 92, 100, 0.9);
+  .logo {
+    font-size: 24px;
+    color: #fc0;
+    i {
+      font-size: 40px;
+      vertical-align: middle;
+      margin-right: 5px;
+    }
+  }
+}
+.is-active {
+  i {
+    color: inherit !important;
+  }
 }
 </style>
---
+++ src/utils/request.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+// import { getToken } from '../utils/store/savelogin'
+// import { message } from 'antd';
+
+export default function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://liufengmao.cn:8000/api/private/v1/',
+    timeout: 5000
+  })
+
+
+  instance.interceptors.response.use(res => {
+    let status = res.data.meta.status
+    let status2 = res.data.meta.msg;
+    if (status !== 200 && status !== 201 && status2 !== 200) {
+      // message.error('出错了，' + res.data.meta.msg)
+      console.log(res);
+
+    }
+    return res.data
+  })
+
+  //!拦截请求
+  instance.interceptors.request.use(function (res) {
+    // let token = getToken();
+    // res.headers['Authorization'] = token
+    return res
+  }, function (error) {
+    return Promise.reject(error)
+  })
+
+
+  return instance(config);
+}
