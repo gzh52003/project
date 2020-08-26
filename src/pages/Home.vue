2e0b8c3ac76f9e3454ee849c4b058e354a2e4454
hongge <1226514725@qq.com>
2020-08-26 14:40:37 +0800

    更新项目

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -25,23 +25,6 @@
               <ve-ring :data="chartDatd" :settings="chartSettings"></ve-ring>
             </div>
           </el-col>
-          <el-col :span="4">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-          <!-- <el-col :span="4">
-            <div   class="grid-content bg-purple"></div>
-          </el-col>-->
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="4">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-          <el-col :span="16">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-          <el-col :span="4">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
         </el-row>
       </div>
     </section>
